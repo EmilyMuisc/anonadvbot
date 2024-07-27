30b0a3705e19ae37d37973e389bf358742cacd57
tigerxmusic <162013460+tigerxmusic@users.noreply.github.com>
2024-07-27 12:23:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ async def start(client, message):
     print("started_sending")
 
     
-    join_buttons = [[InlineKeyboardButton("Join Channel", url=link) for i, (channel_id, link) in enumerate(channel_dictionary.items())]
+    join_buttons = [[InlineKeyboardButton("Join Channel", url=link)] for i, (channel_id, link) in enumerate(channel_dictionary.items())]
     
     # Add the verify button
     join_buttons.append([InlineKeyboardButton("Verify", callback_data="verify")])
